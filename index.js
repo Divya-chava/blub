07000858db0563fd5c54221572bc246e7a50d1f5
Divya-chava <113087019+Divya-chava@users.noreply.github.com>
2024-11-25 14:07:28 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,8 @@
+function bulboff(){
+    let i = document.getElementById('img')
+    i.src = "bulboff.jpg";
+}
+function bulbon(){
+    let i = document.getElementById('img');
+    i.src = "bulbon.jpg";
+}
